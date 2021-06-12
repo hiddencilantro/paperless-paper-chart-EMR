1f9d25248fe8b069892f894af0515a0c05d34934
hiddencilantro <stevekim1109@gmail.com>
2021-06-12 18:53:02 -0400

    providers#create & strong params

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -2,4 +2,20 @@ class ProvidersController < ApplicationController
     def new
         @provider = Provider.new
     end
+
+    def create
+        provider = Provider.new(provider_params)
+        if provider.save
+            session[:user_id] = provider.id
+            redirect_to provider
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def provider_params
+        params.require(:provider).permit(:name, :username, :password, :password_confirm)
+    end
 end
