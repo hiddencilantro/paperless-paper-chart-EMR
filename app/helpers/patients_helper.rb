57113ada690427b71fe7053bf7681e37b3ac45f9
hiddencilantro <stevekim1109@gmail.com>
2021-06-17 14:52:44 -0400

    age helper method

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,2 +1,6 @@
 module PatientsHelper
+    def age(dob)
+        now = Time.zone.now.to_date
+        now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1 )
+    end
 end
