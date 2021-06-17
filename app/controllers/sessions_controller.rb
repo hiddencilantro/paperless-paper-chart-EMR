d09273077e11c53147e74583e4acd70dd842ff84
hiddencilantro <stevekim1109@gmail.com>
2021-06-17 16:27:23 -0400

    provider session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+    def provider_login
+        render :login
+    end
+    
+    def provider_authenticate
+        provider = Provider.find_by(username: params[:username])
+        if provider && provider.authenticate(params[:password])
+            session[:user_id] = provider.id
+            redirect_to provider_path(provider)
+        else
+            render :login
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static#main'
 
+  get '/providers/login', to: 'sessions#provider_login'
+  post '/providers/login', to: 'sessions#provider_authenticate'
+
   resources :providers, only: [:new, :create, :show] do
     resources :patients, only: [:index, :show]
   end
-  
-  get '/login', to: 'sessions#new'
+
 end
