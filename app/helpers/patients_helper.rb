1c659703cf9f8633dc658be7daad9fb850b8440e
hiddencilantro <stevekim1109@gmail.com>
2021-06-18 16:14:11 -0400

    additional conditionals to age method and titleized sex output

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,6 +1,12 @@
 module PatientsHelper
     def age(dob)
         now = Time.zone.now.to_date
-        now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1 )
+        if now.year == dob.year && now.month > dob.month
+            "#{pluralize((now.month - dob.month), 'month')}"
+        elsif now.year == dob.year && now.month == dob.month
+            "#{pluralize((now.day - dob.day), 'day')}"
+        else
+            now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1 )
+        end
     end
 end
