badc041bfd2ca748df42b6f4ecbff339c611458e
hiddencilantro <stevekim1109@gmail.com>
2021-06-25 17:50:08 -0400

    allow underscore for username

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -8,7 +8,7 @@ class Patient < ApplicationRecord
     validates :last_name, presence: true, format: {with: /\A[-a-z A-Z']+\z/, message: "only accepts letters, spaces, hyphens and apostrophes"}
     validates :sex, presence: true
     validates :dob, presence: true
-    validates :username, presence: true, uniqueness: true, format: {with: /\A[0-9a-zA-Z]+\z/, message: "only accepts letters and numbers"}, length: {in: 6..30}, unless: :is_provider
+    validates :username, presence: true, uniqueness: true, format: {with: /\A[0-9a-zA-Z_]+\z/, message: "only accepts letters and numbers"}, length: {in: 6..30}, unless: :is_provider
     validates :password, presence: true, confirmation: true, unless: :is_provider
     validates :password_confirmation, presence: true, unless: :is_provider
     validate :password_requirements, unless: -> {password.blank?}
