d30fa8ad7f8bac5128f9b275f26021607a39ef69
hiddencilantro <stevekim1109@gmail.com>
2021-06-26 15:48:57 -0400

    mass assignment for search action, created edit & update action, improved logic for user flow of creating new patient files & accounts

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,5 @@
 class PatientsController < ApplicationController
-    before_action :verify_if_logged_in, only: [:index, :show, :search, :new], unless: :path_exception
+    before_action :verify_if_logged_in, only: [:index, :search, :new, :edit, :show], unless: :path_exception
     before_action :verify_provider, only: [:index, :search, :new], unless: :path_exception
 
     def index
@@ -7,7 +7,7 @@ class PatientsController < ApplicationController
     end
 
     def search
-        @patient = Patient.find_by(first_name: search_params[:first_name], last_name: search_params[:last_name])
+        @patient = Patient.find_by(search_params) #use .where and .sole to retrieve multiple records
         if @patient
             render :show
         else
@@ -17,8 +17,10 @@ class PatientsController < ApplicationController
     
     def new
         @patient = Patient.new
-        if params[:provider_id]
-            redirect_back fallback_location: provider_patients_path(current_user), allow_other_host: false, flash: {message: "You cannot create a patient file for another provider."} unless params[:provider_id].to_i == current_user.id
+        if logged_in? && current_user.provider?
+            redirect_to provider_patients_path(current_user), flash: {message: "You cannot create a patient file as another provider."} unless params[:provider_id].to_i == current_user.id || path_exception
+        elsif logged_in? && current_user.patient?
+            redirect_to current_user, flash: {message: "You must log out to create a new patient account."}
         end
     end
 
@@ -32,12 +34,52 @@ class PatientsController < ApplicationController
                 render :new
             end
         else
-            @patient = Patient.find_or_create_by(patient_account_params)
-            if @patient.valid?
-                session[:patient_id] = @patient.id
+            @patient = Patient.find_by(first_name: patient_account_params[:first_name], last_name: patient_account_params[:last_name], sex: patient_account_params[:sex], dob: date_reassembled_from_params)
+            if @patient && @patient.username.blank?
+                @patient.assign_attributes(patient_account_params)
+                if @patient.save
+                    session[:patient_id] = @patient.id
+                    redirect_to @patient
+                else
+                    render template: 'patients/_form', locals: {path: @patient, title: "Create a new patient account"}
+                end
+            elsif @patient && @patient.username
+                redirect_to patients_login_path, flash: {message: "Looks like you already have an account! Please sign in to continue."}
+            else
+                @patient = Patient.new(patient_account_params)
+                if @patient.save
+                    session[:patient_id] = @patient.id
+                    redirect_to @patient
+                else
+                    render :new
+                end
+            end
+        end
+    end
+
+    def edit
+        @patient = Patient.find_by(id: params[:id])
+        if !current_user.provider? && !current_user?(@patient)
+            redirect_to current_user, flash: {message: "You cannot edit other patients' information."}
+        end
+    end
+
+    def update
+        @patient = Patient.find_by(id: params[:id])
+        if logged_in? && current_user.provider?
+            @patient.assign_attributes(patient_file_params)
+            if @patient.save
                 redirect_to @patient
             else
-                render :new
+                render :edit
+            end
+        else
+            @patient.assign_attributes(patient_account_params)
+            if @patient.save
+                session[:patient_id] = @patient.id if !logged_in?
+                redirect_to @patient
+            else
+                render :edit
             end
         end
     end
@@ -45,7 +87,7 @@ class PatientsController < ApplicationController
     def show
         @patient = Patient.find_by(id: params[:id])
         if !current_user.provider? && !current_user?(@patient)
-            redirect_back fallback_location: current_user, allow_other_host: false, flash: {message: "You do not have access to other patient files."}
+            redirect_to current_user, flash: {message: "You do not have access to other patient files."}
         end
     end
 
@@ -65,5 +107,9 @@ class PatientsController < ApplicationController
 
     def path_exception
         request.env['PATH_INFO'] == "/patients/new"
+    end
+
+    def date_reassembled_from_params
+        Date.new(patient_account_params["dob(1i)"].to_i, patient_account_params["dob(2i)"].to_i, patient_account_params["dob(3i)"].to_i)
     end
 end
