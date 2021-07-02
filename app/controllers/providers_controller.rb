3b6aea8a3f401a62b00ed399fff2f9860396481c
hiddencilantro <stevekim1109@gmail.com>
2021-07-02 11:24:34 -0400

    before_action set_provider

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -24,12 +24,10 @@ class ProvidersController < ApplicationController
     end
 
     def show
-        set_provider
         redirect_back fallback_location: current_user, allow_other_host: false, flash: {message: "You cannot access another provider's account."} if !current_user?(@provider)
     end
 
     def destroy
-        set_provider
         redirect_back fallback_location: current_user, allow_other_host: false, flash: {message: "You cannot delete another provider's account."} if !current_user?(@provider)
         @provider.destroy
         redirect_to root_path
