048c600457470a64459bf4d69a5cf6bd34a704d6
hiddencilantro <stevekim1109@gmail.com>
2021-07-08 18:20:03 -0400

    combined before_action to one line & error handling

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -1,25 +1,29 @@
 class EncountersController < ApplicationController
-    before_action :verify_if_logged_in
-    before_action :authorize_provider
-    before_action :set_patient_by_id
+    before_action :verify_if_logged_in, :authorize_provider, :set_patient_by_id, :view_all_patient_encounters
     
     def index
-        @encounters = @patient.encounters
         @encounter = Encounter.new
     end
 
     def create
-        encounter = Encounter.new(encounter_params)
-        encounter.provider = current_user
-        encounter.patient = @patient
-        encounter.save
-        redirect_to new_patient_encounter_soap_path(@patient, encounter)
+        @encounter = Encounter.new(encounter_params)
+        @encounter.provider = current_user
+        @encounter.patient = @patient
+        if @encounter.save
+            redirect_to new_patient_encounter_soap_path(@patient, encounter)
+        else
+            render :index
+        end
     end
 
     private
 
     def set_patient_by_id
         @patient = Patient.find_by(id: params[:patient_id])
+    end
+
+    def view_all_patient_encounters
+        @encounters = @patient.encounters
     end
 
     def encounter_params
