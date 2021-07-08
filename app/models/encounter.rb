c1fb712a17defafb1f1854220d185ee386154e46
hiddencilantro <stevekim1109@gmail.com>
2021-07-08 18:17:57 -0400

    added presence validation for encounter_type

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -3,4 +3,5 @@ class Encounter < ApplicationRecord
   belongs_to :patient
   has_one :soap
   enum encounter_type: {soap: 0, physical: 1, well_child: 2}
+  validates :encounter_type, presence: true
 end
