67e364b89a660449fa6fe8470edf23f861935420
hiddencilantro <stevekim1109@gmail.com>
2021-10-30 18:02:56 -0400

    added 'inclusion' to validation of encounter_type

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -4,6 +4,6 @@ class Encounter < ApplicationRecord
   has_one :soap, dependent: :destroy
   accepts_nested_attributes_for :soap
   enum encounter_type: {soap: 0, physical: 1, well_child: 2}
-  validates :encounter_type, presence: true
+  validates :encounter_type, presence: true, inclusion: { in: ["soap", "physical", "well_child"]}
   scope :ordered_by_most_recent, -> {order(created_at: :desc)}
 end
