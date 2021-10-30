a95a1621f2b57f2cddd759bfb616e32b9caa7140
hiddencilantro <stevekim1109@gmail.com>
2021-10-30 17:58:50 -0400

    added helper methods for encounter options & formatting date/time, extracted rendering @encounters to use a partial, renamed 'new' partial to 'type', moved form and show partials for SOAP to soaps directory

--- app/helpers/encounters_helper.rb
+++ app/helpers/encounters_helper.rb
@@ -1,2 +1,13 @@
 module EncountersHelper
+    def encounter_options
+        [["SOAP", "soap"], ["Physical", "physical"], ["Well Child", "well_child"]]
+    end
+
+    def formatted_date(datetime)
+        Time.zone.at(datetime).strftime("%Y-%-m-%-d")
+    end
+
+    def formatted_time(datetime)
+        Time.zone.at(datetime).strftime("%l:%M %p")
+    end
 end
