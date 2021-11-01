51c06edf494f51b786c1af689ddad540f48390b3
hiddencilantro <stevekim1109@gmail.com>
2021-11-01 17:04:59 -0400

    refactored encounter form using helper methods and locals

--- app/helpers/encounters_helper.rb
+++ app/helpers/encounters_helper.rb
@@ -3,6 +3,26 @@ module EncountersHelper
         [["SOAP", "soap"], ["Physical", "physical"], ["Well Child", "well_child"]]
     end
 
+    def new_encounter
+        if params[:encounter_type] == "soap"
+            content_tag(:h2, "SOAP")
+        elsif params[:encounter_type] == "physical"
+            content_tag(:h2, "Physical")
+        elsif params[:encounter_type] == "well_child"
+            content_tag(:h2, "Well Child")
+        end
+    end
+
+    def edit_encounter
+        if @encounter.encounter_type == "soap"
+            content_tag(:h2, "Edit SOAP")
+        elsif @encounter.encounter_type == "physical"
+            content_tag(:h2, "Edit Physical")
+        elsif @encounter.encounter_type == "well_child"
+            content_tag(:h2, "Edit Well Child")
+        end
+    end
+
     def formatted_date(datetime)
         Time.zone.at(datetime).strftime("%Y-%-m-%-d")
     end
