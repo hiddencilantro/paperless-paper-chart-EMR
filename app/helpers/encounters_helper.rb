3e2a9933238bf1dc07b1da08e52bf3f3993e7d89
hiddencilantro <stevekim1109@gmail.com>
2021-11-11 18:20:58 -0500

    renamed helper method

--- app/helpers/encounters_helper.rb
+++ app/helpers/encounters_helper.rb
@@ -3,7 +3,7 @@ module EncountersHelper
         [["SOAP", "soap"], ["Physical", "physical"], ["Well Child", "well_child"]]
     end
 
-    def new_encounter_breadcrumb
+    def new_encounter_text
         if params[:encounter_type] == "soap"
             "New SOAP"
         elsif params[:encounter_type] == "physical"
@@ -13,7 +13,7 @@ module EncountersHelper
         end
     end
 
-    def new_encounter
+    def new_encounter_header
         if params[:encounter_type] == "soap"
             content_tag(:h1, "New SOAP", id: "temp")
         elsif params[:encounter_type] == "physical"
@@ -23,23 +23,23 @@ module EncountersHelper
         end
     end
 
-    def edit_encounter
-        if @encounter.encounter_type == "soap"
-            content_tag(:h1, "Edit SOAP", id: "temp")
-        elsif @encounter.encounter_type == "physical"
-            content_tag(:h1, "Edit Physical", id: "temp")
-        elsif @encounter.encounter_type == "well_child"
-            content_tag(:h1, "Edit Well Child", id: "temp")
-        end
-    end
-
-    def edit_encounter_button
+    def edit_encounter_text
         if @encounter.encounter_type == "soap"
             "Edit SOAP"
         elsif @encounter.encounter_type == "physical"
             "Edit Physical"
         elsif @encounter.encounter_type == "well_child"
             "Edit Well Child"
+        end
+    end
+
+    def edit_encounter_header
+        if @encounter.encounter_type == "soap"
+            content_tag(:h1, "Edit SOAP", id: "temp")
+        elsif @encounter.encounter_type == "physical"
+            content_tag(:h1, "Edit Physical", id: "temp")
+        elsif @encounter.encounter_type == "well_child"
+            content_tag(:h1, "Edit Well Child", id: "temp")
         end
     end
 
