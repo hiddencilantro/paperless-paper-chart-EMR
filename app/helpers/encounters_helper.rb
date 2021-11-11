5bea693035fdd313270c347917818c1943b7f1d6
hiddencilantro <stevekim1109@gmail.com>
2021-11-11 08:11:37 -0500

    helper methods to interpolate title & button text

--- app/helpers/encounters_helper.rb
+++ app/helpers/encounters_helper.rb
@@ -3,23 +3,43 @@ module EncountersHelper
         [["SOAP", "soap"], ["Physical", "physical"], ["Well Child", "well_child"]]
     end
 
+    def new_encounter_breadcrumb
+        if params[:encounter_type] == "soap"
+            "New SOAP"
+        elsif params[:encounter_type] == "physical"
+            "New Physical"
+        elsif params[:encounter_type] == "well_child"
+            "New Well Child"
+        end
+    end
+
     def new_encounter
         if params[:encounter_type] == "soap"
-            content_tag(:h2, "SOAP")
+            content_tag(:h1, "New SOAP", class: "temp")
         elsif params[:encounter_type] == "physical"
-            content_tag(:h2, "Physical")
+            content_tag(:h1, "New Physical", class: "temp")
         elsif params[:encounter_type] == "well_child"
-            content_tag(:h2, "Well Child")
+            content_tag(:h1, "New Well Child", class: "temp")
         end
     end
 
     def edit_encounter
         if @encounter.encounter_type == "soap"
-            content_tag(:h2, "Edit SOAP")
+            content_tag(:h1, "Edit SOAP", class: "temp")
         elsif @encounter.encounter_type == "physical"
-            content_tag(:h2, "Edit Physical")
+            content_tag(:h1, "Edit Physical", class: "temp")
         elsif @encounter.encounter_type == "well_child"
-            content_tag(:h2, "Edit Well Child")
+            content_tag(:h1, "Edit Well Child", class: "temp")
+        end
+    end
+
+    def edit_encounter_button
+        if @encounter.encounter_type == "soap"
+            "Edit SOAP"
+        elsif @encounter.encounter_type == "physical"
+            "Edit Physical"
+        elsif @encounter.encounter_type == "well_child"
+            "Edit Well Child"
         end
     end
 
